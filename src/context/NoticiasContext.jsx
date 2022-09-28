c4f2c5e0cee616756c19636e999d9b964ed37a74
Miguel Arenas Villalobos <admin@miguelarenasvillalobos.com>
2022-09-28 03:54:52 -0500

    proyecto listo

---
+++ src/context/NoticiasContext.jsx
@@ -0,0 +1,5 @@
+import { createContext } from 'react'
+
+const NoticiasContext = createContext()
+
+export default NoticiasContext
---
+++ src/provider/NoticiasProvider.jsx
@@ -0,0 +1,61 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react'
+import NoticiasContext from '../context/NoticiasContext';
+
+const NoticiasProvider = ({ children }) => {
+  const [categoria, setCategoria] = useState('general')
+  const [noticias, setNoticias] = useState([])
+  const [pagina, setPagina] = useState(1)
+  const [totalNoticias, setTotalNoticias] = useState(0)
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apikey=${import.meta.env.VITE_NEWS_API_KEY}`
+
+      const { data } = await axios(url)
+
+      setNoticias(data.articles)
+      setTotalNoticias(data.totalResults)
+      setPagina(1)
+    }
+    consultarAPI()
+  }, [categoria])
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=mx&page=${pagina}&category=${categoria}&apikey=${import.meta.env.VITE_NEWS_API_KEY}`
+
+      const { data } = await axios(url)
+
+      setNoticias(data.articles)
+      setTotalNoticias(data.totalResults)
+    }
+    consultarAPI()
+  }, [pagina])
+
+
+  const handleChangeCategoria = e => {
+    setCategoria(e.target.value)
+  }
+
+  const handleChangePagina = (e, value) => {
+    setPagina(value)
+  }
+
+  return(
+    <NoticiasContext.Provider
+      value={{
+        categoria,
+        handleChangeCategoria,
+        noticias,
+        totalNoticias,
+        pagina,
+        handleChangePagina
+      }}
+    >
+      {children}
+    </NoticiasContext.Provider>
+  )
+}
+
+export default NoticiasProvider
